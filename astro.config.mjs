cfeeae5d31ddbe9ee877c7492a1edaf8e543e8ff
Antonio Tirone <tironeantonio@gmail.com>
2025-09-24 12:14:24 +0200

    feat: themes for dark and light code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,13 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://antoniotir.one",
-  integrations: [preact(), sitemap()]
+  integrations: [preact(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      }
+    },
+  },
 });
