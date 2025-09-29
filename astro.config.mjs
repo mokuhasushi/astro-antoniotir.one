b2df1d8a3b41d404fa6da45777f7c730499fb9f1
Antonio Tirone <tironeantonio@gmail.com>
2025-09-29 18:46:29 +0200

    feat: astromdx, embed and retrospective post

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import preact from "@astrojs/preact";
 
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://antoniotir.one",
-  integrations: [preact(), sitemap()],
+  integrations: [preact(), sitemap(), mdx()],
   markdown: {
     shikiConfig: {
       themes: {
